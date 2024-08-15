190e301726fa9f0becd84097033054f64897e5a5
Akansh Singhal <akanshsinghal7@gmail.com>
2024-08-15 14:13:27 +0530

    github validation

--- testplane-tests/example.testplane.ts
+++ testplane-tests/example.testplane.ts
@@ -1,17 +1,20 @@
-
-describe("test", () => {
-    it("example", async ({ browser }) => {
+describe("Github Public Repository", () => {
+    it("Home Page", async ({ browser }) => {
         await browser.url("https://github.com/gemini-testing/testplane");
-        await browser.$("body").assertView("description",
+        await browser.$("body").assertView("Full View Port",
             {
+                ignoreElements: [
+                    "div[data-testid='latest-commit-details']",
+                    "a[class='Link--primary d-flex no-underline']",
+                    "div[class='react-directory-commit-age']"
+                ],
                 ignoreDiffPixelCount: 10,
                 tolerance: 5,
                 antialiasingTolerance: 4,
                 allowViewportOverflow: true,
                 captureElementFromTop: true,
                 compositeImage: true,
-                screenshotDelay: 1000,
-                ignoreElements: ["div[data-testid='latest-commit-details']", "a[class='Link--primary d-flex no-underline']"]
+                screenshotDelay: 1000
             }
         )
     });
